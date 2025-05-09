e84c41c1f195f543d58296b419879679375108f7
Cecelia Moore <143442@Swcsd.us>
2025-05-09 08:15:38 -0400

    Initial Commit

---
+++ Potato/PotatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotatoApp.swift
+//  Potato
+//
+//  Created by Cecelia Moore on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PotatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
