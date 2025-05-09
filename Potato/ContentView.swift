3fdbb4ca9dad00f2af5a867ebd130e4327c8984b
Cecelia Moore <143442@Swcsd.us>
2025-05-09 08:25:12 -0400

    made small change as testy test ( P O T A T O >:) )

--- Potato/ContentView.swift
+++ Potato/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "capsule.portrait.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Cece-Fried Potatoes")
+            Text("Freaky Hugo >:)")
         }
         .padding()
     }
